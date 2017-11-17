9e2a05c8fb8a5772fbc43587c1d8802a5fb76caa
Teemu Kääriä <teemu.kaaria@aalto.fi>
2017-11-17 20:18:47 +0200

    added boundries to draging

--- app/components/timeslot/Timeslot.js
+++ app/components/timeslot/Timeslot.js
@@ -25,8 +25,12 @@ export class Timeslot extends React.Component {
                 end: this.state.available.end
             }
         };
-        if (type === 'start') newAvailable.available.start = this.state.available.start + change * split
-        if (type === 'end') newAvailable.available.end = this.state.available.end + change * split
+        if (type === 'start') {
+            newAvailable.available.start = Math.min(Math.max(this.state.available.start + change * split, parseTimestamp(startTime)), this.state.available.end);
+        }
+        if (type === 'end') {
+             newAvailable.available.end = Math.max(Math.min(this.state.available.end + change * split, parseTimestamp(endTime)), this.state.available.start);
+        }
         this.setState(newAvailable);
     }
     handleManipulationEnd() {
