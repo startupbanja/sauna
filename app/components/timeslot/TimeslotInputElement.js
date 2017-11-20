a2a25622445faa0f157ebbb16b0e367e498c1ec7
Teemu Kääriä <teemu.kaaria@aalto.fi>
2017-11-20 18:11:55 +0200

    added insertion with enter

--- app/components/timeslot/TimeslotInputElement.js
+++ app/components/timeslot/TimeslotInputElement.js
@@ -14,13 +14,25 @@ export class TimeslotInputElement extends Component {
         this.state = {editedValue: ""};
         this.handleChange = this.handleChange.bind(this);
         this.handleBlur = this.handleBlur.bind(this);
+        this.handleKeypress = this.handleKeypress.bind(this);
+        this.endEditing = this.endEditing.bind(this);
     }
 
     handleChange(event) {
         this.setState({editedValue: event.target.value});
+        console.log("change");
     }
     handleBlur(event) {
+        this.endEditing(event);
+    }
+    handleKeypress(event) {
+        if (event.key == 'Enter') {
+            this.endEditing(event);
+        }
+    }
+    endEditing(event) {
         this.setState({editedValue: ""});
+        event.target.blur();
         if (parseMinutes(event.target.value) !== false) {
             this.props.onChange(parseMinutes(event.target.value) - this.props.time);
         }
@@ -30,7 +42,7 @@ export class TimeslotInputElement extends Component {
         var value = parseTimeStamp(this.props.time);
         if (this.state.editedValue !== "") value = this.state.editedValue;
         return (
-            <input onChange={this.handleChange} onBlur={this.handleBlur} style={styles.input} value={value} type="text" />
+            <input onKeyPress={this.handleKeypress} onChange={this.handleChange} onBlur={this.handleBlur} style={styles.input} value={value} type="text" />
         );
     }
 }
