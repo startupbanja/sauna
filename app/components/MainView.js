50595f272c1bfbb244c868c7ad757049a3db5767
Tarmo Asikainen <tare.asikainen@gmail.com>
2017-11-21 16:30:00 +0200

    make empty file pageContent.js

--- app/components/MainView.js
+++ app/components/MainView.js
@@ -2,7 +2,7 @@ import React from "react";
 import Menu from "./Menu.js";
 
 
-//This is the class that shows the whoel page content after login
+//This is the class that shows the whole page content after login
 //Currently shows a menubar at the top and content below it
 export default class MainView extends React.Component {
 
@@ -10,18 +10,17 @@ export default class MainView extends React.Component {
         super(props);
         this.changeContent = this.changeContent.bind(this);
         this.state = {
-            "current" : <h1>Welcome, {this.props.user}</h1>,
-            contentMap: {
+            current : <h1>Welcome, {this.props.user}</h1>,
+            contentMap : {
                 "timetable" : <h1>Timetable</h1>,
                 "otherstuff": <h1>Other stuff</h1>
             },
 
-            labels: {
+            labels : {
                 "timetable" : "Timetable",
                 "otherstuff": "Other Stuff"
             }
         }
-        var landingPage = "timetable";
     }
 
 
