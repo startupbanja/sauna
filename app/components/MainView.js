6d4c895a6934aca0f6cba0a25a5324540ade25dd
Tarmo Asikainen <tare.asikainen@gmail.com>
2017-11-21 16:43:32 +0200

    Add page content information to a separate file

--- app/components/MainView.js
+++ app/components/MainView.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Menu from "./Menu.js";
 import Image from "./Image.js";
+import pageContent from "./pageContent.js";
 
 
 //This is the class that shows the whole page content after login
@@ -12,20 +13,8 @@ export default class MainView extends React.Component {
         this.changeContent = this.changeContent.bind(this);
         this.state = {
             "current" : <div><h1>Welcome, {this.props.user}</h1><Image src="../app/imgs/firstScreen1.png"/></div>,
-            contentMap: {
-                "mainPage" : <div><h1>Home</h1><Image src="../app/imgs/firstScreen1.png"/></div>,
-                "timetable" : <div><h1>Timetable</h1><Image src="../app/imgs/userSchedule1.png"/></div>,
-                "userProfile" : <div><h1>User Profile</h1><Image src="../app/imgs/userProfile1.png"/></div>,
-                "feedback" : <div><h1>Feedback Forms</h1><Image src="../app/imgs/feedback2.png"/></div>,
-                "coaches" : <div><h1>Coach Information</h1><Image src="../app/imgs/coaches1.png"/></div>,
-            },
-            labels: {
-                "mainPage" : "Home",
-                "timetable" : "Timetable",
-                "userProfile" : "User Profile",
-                "feedback" : "Feedback",
-                "coaches" : "Coaches"
-            }
+            contentMap: pageContent.contentMap,
+            labels: pageContent.labels
         }
     }
 
