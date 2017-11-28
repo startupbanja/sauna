1bef61781b5f6a61dc8134478cd7c46967db0682
Niilo Salo <niilo.salo@aalto.fi>
2017-11-28 17:00:34 +0200

    Began working on user profile

---
+++ app/components/UserProfile.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default class UserProfile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: "Tuukka"
+    };
+    this.getHeader = this.getHeader.bind(this)
+  }
+
+  getHeader() {
+    return (
+      <h1>Your profile, {this.state.userName}</h1>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+      {this.getHeader()}
+      </div>
+    );
+  }
+}
--- app/components/pageContent.js
+++ app/components/pageContent.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Image from "./Image.js";
+import UserProfile from "./UserProfile.js";
 
 
 var content = {
     "mainPage" : <div><h1>Home</h1><Image src="../app/imgs/firstScreen1.png"/></div>,
     "timetable" : <div><h1>Timetable</h1><Image src="../app/imgs/userSchedule1.png"/></div>,
-    "userProfile" : <div><h1>User Profile</h1><Image src="../app/imgs/userProfile1.png"/></div>,
+    "userProfile" : <UserProfile />,
     "feedback" : <div><h1>Feedback Forms</h1><Image src="../app/imgs/feedback2.png"/></div>,
     "coaches" : <div><h1>Coach Information</h1><Image src="../app/imgs/coaches1.png"/></div>
 }
