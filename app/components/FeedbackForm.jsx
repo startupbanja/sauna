7caf2a0fa5cce86aa1d411471239fb474ff82cf9
Valtteri Nurminen <valtteri.e.nurminen@aalto.fi>
2017-12-04 12:46:17 +0200

    Added the files that were on Drive

---
+++ app/components/FeedbackForm.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+/* React Component for feedback forms. */
+class FeedbackForm extends React.Component {
+    
+ render(){
+    return (
+    
+        <div>
+            
+        
+        
+        </div>);
+ }
+    
+}
