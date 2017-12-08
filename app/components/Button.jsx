e356642498ffbd4ea40a685d4bdf593ab4bd7e24
Tarmo Asikainen <tare.asikainen@gmail.com>
2017-12-08 15:59:10 +0200

    work on menu, now mostly works, links are a bit ugly

--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -5,12 +5,18 @@ import PropTypes from 'prop-types';
 export default class Button extends React.Component {
   render() {
     return (
-      <button onClick={this.props.onClick}>{this.props.text}</button>
+      <button
+        className={this.props.className}
+        onClick={this.props.onClick}
+      >
+        {this.props.text}
+      </button>
     );
   }
 }
 
 Button.propTypes = {
+  className: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
   text: PropTypes.string.isRequired,
 };
--- app/components/Menu.jsx
+++ app/components/Menu.jsx
@@ -1,33 +1,55 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from './Button';
+// import Button from './Button';
 
 export default class Menu extends React.Component {
   render() {
     const list = Object.keys(this.props.content).map(name => (
-      <li key={name}>
-        <Button
+      <li key={name} className="nav-item menuListItem" >
+        <a
+          className="nav-link"
+          href="#"
           onClick={() => { this.props.onChange(name); }}
-          text={this.props.content[name]}
-        />
+        >
+          {this.props.content[name]}
+        </a>
       </li>
     ));
-    // for (const name in this.props.content) {
-    //   list.push(
-    //     <li key={name}>
-    //       <Button
-    //         onClick={() => { this.props.onChange(name); }}
-    //         text={this.props.content[name]} />
-    //   </li>)
-    // }
     return (
-      <div>
-        <ul>{list}</ul>
-        <Button
-          onClick={this.props.logoff}
-          text="Log Off"
-        />
-      </div>
+      <nav className="navbar navbar-toggleable-md navbar-inverse bg-inverse fixed-top menubar">
+        <div className="ads">
+          <div className="navbar-header">
+            <button
+              className="navbar-toggle collapsed"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarMenu"
+              aria-controls="navbarMenu"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar" />
+              <span className="icon-bar" />
+              <span className="icon-bar" />
+            </button>
+            <a className="navbar-brand" href="#">Sauna</a>
+          </div>
+          <div className="navbar-collapse collapse" id="navbarMenu">
+            <ul className="navbar-nav mr-auto">
+              {list}
+              <a
+                className="menubutton nav-button"
+                href="#"
+                onClick={this.props.logoff}
+              >
+                Log Off
+              </a>
+            </ul>
+          </div>
+        </div>
+      </nav>
     );
   }
 }
@@ -37,3 +59,10 @@ Menu.propTypes = {
   content: PropTypes.objectOf(PropTypes.string).isRequired,
   onChange: PropTypes.func.isRequired,
 };
+
+//
+// <Button
+//   className="menubutton btn"
+//   onClick={() => { this.props.onChange(name); }}
+//   text={this.props.content[name]}
+// />
