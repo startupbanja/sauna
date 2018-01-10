6f5655830c4176034180574fa83fecf1bfae6dfb
Esa <esa.vikberg@outlook.com>
2018-01-10 13:23:29 +0200

    Removed obsolete folder

--- backend/README.md
+++
@@ -1,7 +0,0 @@
-Backend toimimaan:
-1. npm install
-2. node index.js
-3. selaimella localhost:3000
-    -   palauttaa index.html
-4. selaimella localhost:3000/api?q=<insert jotain hauskaa>
-    -   palauttaa JSON
--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,74 @@
-var express    = require('express');
-var app        = express();
-var bodyParser = require('body-parser');
+const express = require('express');
+const readline = require('readline');
+const bodyParser = require('body-parser');
+const database = require('./database.js');
+
+const app = express();
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(function(req, res, next) {
     console.log('Something is happening.');
     next();
 });
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     res.sendFile(__dirname+"/index.html");
 });
 
-app.get('/api', function(req, res) {
-    if (req.query.hasOwnProperty("q")) {
-        res.json({ message: req.query.q });
-    } else {
-        res.json({ message: 'Hello world!' });
-    }
+app.get('/api', (req, res) => {
+  if (req.query.hasOwnProperty("q")) {
+    res.json({ message: req.query.q });
+  } else {
+    database.getUsers(1, 0, (data) => {
+      res.json(data);
+    });
+    // database.testApi((data) => {
+    //   res.json(data);
+    // });
+  }
 });
 
-app.get('/api', function(req, res) {
-    res.json({ message: req.query.q });   
-});
+// app.get('/api', function(req, res) {
+//     res.json({ message: req.query.q });
+// });
 
-app.listen(port);
+const server = app.listen(port);
 console.log('Magic happens on port ' + port);
 
+function closeServer() {
+  database.closeDatabase(() => {
+    server.close(() => {
+      console.log('HTTP Server closed.\nExiting...');
+      process.exit();
+    });
+  });
+}
 
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on('line', (input) => {
+  if (input === 'exit') {
+    closeServer();
+  }
+});
+
+// make SIGINT work on both windows and linux with the readline module
+rl.on('SIGINT', () => {
+  process.emit('SIGINT');
+});
+
+// Catch interrupt signal(CTRL+C)
+// process is a node global variable
+process.on('SIGINT', () => {
+  console.log('\nCaught interrupt signal');
+  closeServer();
+});
