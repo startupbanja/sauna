7efb79cbd97e08860646aecc48e6260eb2dd8aea
Tarmo Asikainen <tare.asikainen@gmail.com>
2018-01-10 13:33:34 +0200

    try to get json stuff to work

--- Backend/matchmaking.js
+++ Backend/matchmaking.js
@@ -3,10 +3,12 @@ const childProcess = require('child_process');
 
 function runMatchmaking() {
   const filename = 'test.py';
+  const jsonData = { feedbacks: [], availabilities: [] };
   const newProcess = childProcess.spawn('python', [filename]);
   newProcess.stdout.on('data', (data) => {
     console.log(data.toString());
   });
+  newProcess.stdin.write(JSON.stringify(jsonData));
 }
 
 module.exports = {
--- Backend/test.py
+++ Backend/test.py
@@ -1,1 +1,8 @@
+import json
+import sys
+
 print("testing test.py")
+
+data = json.load(sys.stdin)
+
+print(json.dumps(data))
