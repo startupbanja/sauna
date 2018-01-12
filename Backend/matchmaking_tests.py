9da371a8686885ed863fcc6567a92c7797be8f32
Tarmo Asikainen <tare.asikainen@gmail.com>
2018-01-12 15:07:20 +0200

    start writing unit tests

---
+++ Backend/matchmaking_tests.py
@@ -0,0 +1,35 @@
+import unittest
+import matchmaking
+
+class TestMatchmaking(unittest.TestCase):
+
+  def setUp(self):
+    self.dictA = {
+      startup: 1,
+      startupfeedback: 2,
+      coach: 2,
+      coachfeedback: 2
+    }
+    self.dictB = {
+      startup: 2,
+      startupfeedback: 1,
+      coach: 1,
+      coachfeedback: 1
+    }
+
+  def tearDown(self):
+
+  def test_getSum(self):
+    self.assertEqual(matchmaking.getSum(0,0),0)
+    self.assertEqual(matchmaking.getSum(0,1),1)
+    self.assertEqual(matchmaking.getSum(-1,0),0)
+    self.assertEqual(matchmaking.getSum(-1,-1),2.5)
+    self.assertEqual(matchmaking.getSum(2,3),5)
+    self.assertEqual(matchmaking.getSum(-1,2),2)
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
