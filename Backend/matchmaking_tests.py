8e563d4f6a1eaca9aa85e000b4d7a3f007888e5c
Tarmo Asikainen <tare.asikainen@gmail.com>
2018-01-17 14:25:23 +0200

    now algorithm should work

--- Backend/matchmaking_tests.py
+++ Backend/matchmaking_tests.py
@@ -1,21 +1,29 @@
 import unittest
 import matchmaking
+import datetime
+import random
 
 class TestMatchmaking(unittest.TestCase):
 
   def setUp(self):
-    self.dictA = {
-      'startup': 1,
-      'startupfeedback': 2,
-      'coach': 2,
-      'coachfeedback': 2
-    }
-    self.dictB = {
-      'startup': 2,
-      'startupfeedback': 1,
-      'coach': 1,
-      'coachfeedback': 1
-    }
+    random.seed(1)
+    self.feedbacks = []
+    for i in range(10):
+      for j in range(10):
+        self.feedbacks.append({
+          'startup': i,
+          'startupfeedback': random.choice([-1,0,1,3]),
+          'coach': j,
+          'coachfeedback': random.choice([-1,0,1,2])
+        })
+    self.availabilities = {}
+    for i in range(10):
+      self.availabilities[i] = {
+      'starttime': str(random.randrange(9,12))+ ":00:00",
+      'duration': random.randrange(0, 240, 40)
+      }
+    testData = {'feedbacks': self.feedbacks, 'availabilities': self.availabilities}
+    self.paramTuple = matchmaking.init(True, testData)
 
   def tearDown(self):
     pass
@@ -27,6 +35,9 @@ class TestMatchmaking(unittest.TestCase):
     self.assertEqual(matchmaking.getSum(-1,-1),2.5)
     self.assertEqual(matchmaking.getSum(2,3),5)
     self.assertEqual(matchmaking.getSum(-1,2),2)
+
+  def test_matchmake(self):
+    print(matchmaking.matchmake(self.paramTuple[0], self.paramTuple[1], self.paramTuple[2]))
 
 
 
