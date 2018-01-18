6e017712da5d1176f2714f1138c7ed4c805519dd
Esa <esa.vikberg@outlook.com>
2018-01-18 15:10:10 +0200

    Added a few python unit tests

--- Backend/matchmaking_tests.py
+++ Backend/matchmaking_tests.py
@@ -24,6 +24,34 @@ class TestMatchmaking(unittest.TestCase):
       }
     testData = {'feedbacks': self.feedbacks, 'availabilities': self.availabilities}
     self.paramTuple = matchmaking.init(True, testData)
+    self.definedFeedbacks = [];
+    userid = 0;
+    for i in [-1,0,1,3]:
+        userid += 1
+        for j in [-1,0,1,2]:
+            self.definedFeedbacks.append({
+              'startup': userid,
+              'startupfeedback': i,
+              'coach': userid,
+              'coachfeedback': j
+            })
+    self.definedAvailabilities = {};
+    self.definedAvailabilities[1] = {
+      'starttime': "09:00:00",
+      'duration': 240
+    }
+    self.definedAvailabilities[2] = {
+      'starttime': "09:00:00",
+      'duration': 120
+    }
+    self.definedAvailabilities[3] = {
+      'starttime': "10:00:00",
+      'duration': 120
+    }
+    definedTestData = {'feedbacks': self.definedFeedbacks, 'availabilities': self.definedAvailabilities}
+    self.definedParamTuple = matchmaking.init(True, definedTestData)
+    self.definedAvailabilities = self.definedParamTuple[1]
+
 
   def tearDown(self):
     pass
@@ -39,7 +67,20 @@ class TestMatchmaking(unittest.TestCase):
   def test_matchmake(self):
     print(matchmaking.matchmake(self.paramTuple[0], self.paramTuple[1], self.paramTuple[2]))
 
+  def test_cmpByFeedback(self):
+    self.assertEqual(matchmaking.cmpByFeedback(self.definedFeedbacks[0],self.definedFeedbacks[0]),0)
+    self.assertEqual(matchmaking.cmpByFeedback(self.definedFeedbacks[0],self.definedFeedbacks[4]),2)
+    self.assertEqual(matchmaking.cmpByFeedback(self.definedFeedbacks[0],self.definedFeedbacks[8]),1)
+    self.assertEqual(matchmaking.cmpByFeedback(self.definedFeedbacks[0],self.definedFeedbacks[12]),0)
+    self.assertEqual(matchmaking.cmpByFeedback(self.definedFeedbacks[4],self.definedFeedbacks[0]),-2)
+    self.assertEqual(matchmaking.cmpByFeedback(self.definedFeedbacks[8],self.definedFeedbacks[0]),-1)
+    self.assertEqual(matchmaking.cmpByFeedback(self.definedFeedbacks[12],self.definedFeedbacks[0]),0)
 
+  def test_cmpByTimestart(self):
+    self.assertEqual(matchmaking.cmpByTimestart(self.definedFeedbacks[0],self.definedFeedbacks[0],self.definedAvailabilities),0)
+    self.assertEqual(matchmaking.cmpByTimestart(self.definedFeedbacks[0],self.definedFeedbacks[4],self.definedAvailabilities),0)
+    self.assertEqual(matchmaking.cmpByTimestart(self.definedFeedbacks[0],self.definedFeedbacks[8],self.definedAvailabilities),-3600)
+    self.assertEqual(matchmaking.cmpByTimestart(self.definedFeedbacks[0],self.definedFeedbacks[8],self.definedAvailabilities),3600)
 
 
 
