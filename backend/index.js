929ef7eceea78142e25908a148cda161ee2cc196
Esa <esa.vikberg@outlook.com>
2018-02-22 16:51:37 +0200

    fixed indentation

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-  const express = require('express');
+const express = require('express');
 const readline = require('readline');
 const bodyParser = require('body-parser');
 const database = require('./database.js');
