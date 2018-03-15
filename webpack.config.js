bc448c1e52c0d22b619d13466c5ac36f9a93e71d
Tarmo Asikainen <tare.asikainen@gmail.com>
2018-03-15 17:21:00 +0200

    make modifications to route /api fetches from frontend correctly to backend for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,13 @@ module.exports = {
     ],
     devServer: {
         historyApiFallback: true,
+        proxy: {
+          '/api/': {
+            target: 'http://127.0.0.1:3000/',
+            secure: false,
+            pathRewrite: {'^/api/' : '/'}
+          }
+        },
     }
 };
 /* eslint-enable */
