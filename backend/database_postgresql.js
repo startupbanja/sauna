404fee1335d71a3cff4cd8752d8e61b9a76370ae
Esa <esa.vikberg@outlook.com>
2018-03-22 12:35:21 +0200

    implemented getUsers for database in PostgreSQL

--- backend/database_postgresql.js
+++ backend/database_postgresql.js
@@ -10,13 +10,47 @@ const params = {
 
 function loadPassword() {
   fs.readFile('./db_password.txt', 'utf8', (err, data) => {
+    if (err) console.error(err);
     const pwdString = data.split('\n')[0];
     params.password = pwdString;
   });
 }
 
+loadPassword();
+
 function getClient() {
-  return pg.Client(params);
+  return new pg.Client(params);
 }
 
-loadPassword();
+function getUsers(type, includeId, callback) {
+  const client = getClient();
+  const users = {};
+  const query = {
+    name: 'get-users',
+    text: `
+    SELECT id, name, description, img_url
+    FROM Users
+    LEFT OUTER JOIN Profiles ON Profiles.user_id = Users.id
+    WHERE type = $1 AND active = TRUE;`,
+    values: [type],
+  };
+  client.connect((err) => {
+    if (err) callback(err);
+    else {
+      client.query(query, (err2, res) => {
+        if (err2) callback(err2);
+        else {
+          res.rows.forEach((row) => {
+            users[row.name] = {
+              description: row.description,
+              img_url: row.img_url,
+            };
+            if (includeId) users[row.name].id = row.user_id;
+          });
+          callback(err2, users);
+        }
+        client.end();
+      });
+    }
+  });
+}
